cb260cc439aa08abfd676fb0be31aadf3e725cad
User <User@DESKTOP-L9LEDCK>
2019-02-13 00:44:22 +0300

    1 версия программы

---
+++ ConsoleApp1/Params_of_notes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Params_of_notes
+    {
+       public string title ;
+       public string body  ;
+       public int id = 0;
+       public DateTime time;
+
+        
+        
+
+        public void Show() { Console.WriteLine($"{title} - {body} - {id} - {time}"); } 
+
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Params_of_notes params_of_notes;
+            int a = 1;
+            string text_for_title = "Test title";
+            string text_for_body = "Test body";
+            string b;
+            while (a != 0){
+
+                b = Convert.ToString(a);
+                params_of_notes.title = (text_for_title +" "+ b);
+                params_of_notes.body = (text_for_body+" "+  b);
+                params_of_notes.Show(); 
+                    a++;
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
