90326eb6f6f96d4b5968596da108be011cf3d2b2
User <User@DESKTOP-L9LEDCK>
2019-02-27 15:05:06 +0300

    не получилось

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+
 
 namespace ConsoleApp1
 {
@@ -16,19 +18,20 @@ namespace ConsoleApp1
             string text_for_body = "Test body";
             string text_for_id = "Test id";
              string b;
-            while (a != 0) {
+            /*  while (a != 0) {
 
-                b = Convert.ToString(a);
-                params_of_notes.title = (text_for_title + " " + b);
-                params_of_notes.body = (text_for_body + " " + b);
-                params_of_notes.id = (a);
-                params_of_notes.time = DateTime.Now;
+                  b = Convert.ToString(a);
+                  params_of_notes.title = (text_for_title + " " + b);
+                  params_of_notes.body = (text_for_body + " " + b);
+                  params_of_notes.id = (a);
+                  params_of_notes.time = DateTime.Now;
 
-                params_of_notes.Show();
-                a++;
-            }
-        
-            for (int i = 1; i != 0; i++)
+                  params_of_notes.Show();
+                  a++;
+              }
+              */
+            Params_of_notes[] notes = new Params_of_notes[100];
+            for (int i = 0; i <100; i++)
             {
                 b = Convert.ToString(i);
                 params_of_notes.title = (text_for_title + " " + b);
@@ -39,8 +42,11 @@ namespace ConsoleApp1
 
                 params_of_notes.Show();
                 i++;
+                notes[i] = params_of_notes; 
+                
+                
 
-                foreach (string parametry in params_of_notes) { Console.WriteLine(parametry); }
+
 
 
 
@@ -48,6 +54,8 @@ namespace ConsoleApp1
             }
 
 
+            var zapakovka = JsonConvert.SerializeObject(notes);
+            var path = JsonConvert.DeserializeObject<notes>(zapakovka);
 
 
 
